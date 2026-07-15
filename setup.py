cc31fd609f0516e7755d917d90cceb6c58b85773
muhammad zohaib <zohaibafridi134567.com>
2026-07-15 10:41:04 +0500

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="zohaib",
+    author_email="zohaibafridi1234567@gmail.com",
+    packages=find_packages()
+)
