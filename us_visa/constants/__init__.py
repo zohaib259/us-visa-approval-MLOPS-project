fb4c7c95af75b33b4945c61b8d2035b75025da43
muhammad zohaib <zohaibafridi1234567@gmail.com>
2026-07-18 11:45:19 +0500

    added config_entitiy and artifact_entity

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -9,6 +9,10 @@ MONGODB_URL_KEY = "MONGODB_URL"
 PIPELINE_NAME: str = "usvisa"
 ARTIFACT_DIR: str = "artifact"
 
+TRAIN_FILE_NAME = "train.csv"
+TEST_FILE_NAME = "test.csv"
+
+FILE_NAME = "usvia.csv"
 MODEL_FILE_NAME = "model.pkl"
 
 
