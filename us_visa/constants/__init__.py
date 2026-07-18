b7a476a0bc83725324ea70e582892f7bbcffa266
muhammad zohaib <zohaibafridi1234567@gmail.com>
2026-07-18 10:46:56 +0500

    added constant variables

---
+++ us_visa/constants/__init__.py
@@ -0,0 +1,24 @@
+import os 
+from datetime import date
+
+
+DATABASE_NAME = "US_VISA"
+CONNECTION_NAME = "visa_data"
+MONGODB_URL_KEY = "MONGODB_URL"
+
+PIPELINE_NAME: str = "usvisa"
+ARTIFACT_DIR: str = "artifact"
+
+MODEL_FILE_NAME = "model.pkl"
+
+
+"""
+    Data ingestion constants
+"""
+
+DATA_INGESTION_COLLECTION_NAME: str = "visa_data"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
