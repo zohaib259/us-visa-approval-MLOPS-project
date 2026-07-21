08c9b7693384396eb45c6e20f49e844a7af88235
muhammad zohaib <zohaibafridi1234567@gmail.com>
2026-07-21 11:54:38 +0500

    added estimator that will convert target into 0 and 1 and when we enter 0 and 1 it will reverse it and will give us certified or non certifiend during prediction

---
+++ us_visa/entity/estimator.py
@@ -0,0 +1,16 @@
+from us_visa.exception import USvisaException
+from us_visa.logger import logging
+
+
+
+class TargetValueMapping:
+    def __init__(self):
+        self.Certified:int = 0
+        self.Denied:int = 1
+    def _asdict(self):
+        return self.__dict__
+    def reverse_mapping(self):
+        mapping_response = self._asdict()
+        return dict(zip(mapping_response.values(),mapping_response.keys()))
+    
+
