2ddda1c38539d86c15bf195333f8b4d97fd34fa8
muhammad zohaib <zohaibafridi1234567@gmail.com>
2026-07-22 15:13:32 +0500

    added s3 estimator cloud connection congiguration also added asws_storage.py for manipulating s3 bucket

--- us_visa/entity/config_entity.py
+++ us_visa/entity/config_entity.py
@@ -53,3 +53,10 @@ class ModelTrainerConfig:
     expected_accuracy: float = MODEL_TRAINER_EXPECTED_SCORE
     model_config_file_path: str = MODEL_TRAINER_MODEL_CONFIG_FILE_PATH
 
+
+@dataclass
+class ModelEvaluationConfig:
+    changed_threshold_score: float = MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE
+    bucket_name: str = MODEL_BUCKET_NAME
+    s3_model_key_path: str = MODEL_FILE_NAME
+
