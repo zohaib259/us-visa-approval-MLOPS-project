2a9a4dad563718b806264c41c27f7bebc4820871
muhammad zohaib <zohaibafridi1234567@gmail.com>
2026-07-22 12:11:52 +0500

    added model evaluation constant and aws_connection.py

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -23,6 +23,13 @@ FILE_NAME = "usvia.csv"
 MODEL_FILE_NAME = "model.pkl"
 
 
+AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
+AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
+REGION_NAME = "eu-north-1"
+
+
+
+
 """
     Data ingestion constants
 """
@@ -60,3 +67,14 @@ MODEL_TRAINER_TRAINED_MODEL_NAME: str = "model.pkl"
 MODEL_TRAINER_EXPECTED_SCORE: float = 0.6
 MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
 
+
+"""
+MODEL EVALUATION related constant 
+"""
+MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
+MODEL_BUCKET_NAME = "usvisa-model-x-2026"
+MODEL_PUSHER_S3_KEY = "model-registry"
+
+
+APP_HOST = "0.0.0.0"
+APP_PORT = 8080
